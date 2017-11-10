5190cf120d9e5dd6949b9123eb1833f679ff3260
burhanuddin rampurawala <burhanuddinzrampurawala@gmail.com>
2017-11-10 14:54:06 +0000

    file added

---
+++ setUp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+read -p "do you want to install mongodb?[Y/N]:" i
+if [ "$i" == "y" ] || [ "$i" == "Y" ] 
+then
+	echo installing mongodb...
+	sudo apt-get install mongodb
+	echo mongodb installed
+fi
+read -p "do you want to set data/db path for mongodb mongodb?[Y/N]:" i
+if [ "$i" == "y" ] || [ "$i" == "Y" ] 
+then
+	cd  /var/lib/mongodb
+	sudo mkdir -p /data/db/
+	sudo chown `id -u` /data/db
+fi
+read -p "do you want to install nodejs version 8?[Y/N]:" i
+if [ "$i" == "y" ] || [ "$i" == "Y" ] 
+then
+	curl -sL https://deb.nodesource.com/setup_$v | sudo -E bash -
+	sudo apt-get install -y nodejs
+fi
+read -p "do you want to install node-gyp?[Y/N]:" i
+if [ "$i" == "y" ] || [ "$i" == "Y" ] 
+then
+	npm install -g node-gyp
+
+fi
+
+echo node version
+node -v
+echo npm version 
+npm -v
