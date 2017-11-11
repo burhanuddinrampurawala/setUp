74ca0f46260130e98f91ef00f3b48591bce0ff60
burhanuddinrampurawala <burhanuddinzrampurawala@gmail.com>
2017-11-11 13:00:17 +0000

    node issue solved

--- setUp.sh
+++ setUp.sh
@@ -24,7 +24,7 @@ read -p "do you want to install nodejs version 8?[Y/N]:" i
 if [ "$i" == "y" ] || [ "$i" == "Y" ] 
 then
 	echo -e "\033[34m"
-	curl -sL https://deb.nodesource.com/setup_$v | sudo -E bash -
+	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 	sudo apt-get install -y nodejs
 fi
 echo -e "\033[35m"
